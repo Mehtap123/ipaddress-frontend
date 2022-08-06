a7125272f27ec7d5f8b72fde9980fa8a09325bd8
Mehtap <kayhanmehtap10@gmail.com>
2022-08-06 20:38:38 +0200

    add Landingpage and post ip adress to get infomotion about it and show it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Landingpage from "./components/Landingpage";
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path={"/"} element={<Landingpage/>}/>
+      </Routes>
       
     </div>
   );

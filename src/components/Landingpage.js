8087811f16c2c99736e0d48ec86ce4e8a8a07714
Mehtap <kayhanmehtap10@gmail.com>
2022-08-07 16:02:13 +0200

    change initial value of state variable

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -6,7 +6,21 @@ import { TailSpin } from "react-loader-spinner";
 const Landingpage = () => {
   // const api = process.env.REACT_APP_API_URL;
 
-  const [data, setData] = useState({});
+  const [data, setData] = useState({"query": "24.48.0.1",
+  "status": "success",
+  "country": "Canada",
+  "countryCode": "CA",
+  "region": "QC",
+  "regionName": "Quebec",
+  "city": "Montreal",
+  "zip": "H1K",
+  "lat": 45.6085,
+  "lon": -73.5493,
+  "timezone": "America/Toronto",
+  "isp": "Le Groupe Videotron Ltee",
+  "org": "Videotron Ltee",
+  "as": "AS5769 Videotron Telecom Ltee"
+  });
 
   useEffect(() => {
     fetch("http://ip-api.com/json/24.48.0.1")
